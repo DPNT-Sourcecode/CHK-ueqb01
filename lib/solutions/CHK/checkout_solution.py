fec19c15b5e866f2c340c63e3a3e8af7a06c5e65
sbx_user1051 <sbx_user1051@169.254.161.209>
2022-04-21 22:33:22 +0000

    Thu Apr 21 22:33:22 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -6,9 +6,11 @@ def checkout(skus):
         LOWER_PRICE_OFFER = 1
         FREE_SKU_OFFER = 2
         BUY_X_GET_1_FREE = 3
+        GROUP_OFFER = 4
 
     class Offer:
-        def __init__(self, sku: str, offer_type: 'OfferType', trigger_quantity: int, offer_price: int, sku_applied_to: str = None, x: int = None) -> None:
+        def __init__(self, sku: str, offer_type: 'OfferType', trigger_quantity: int, offer_price: 
+                            int, sku_applied_to: str = None, x: int = None, group = ['S', 'T', 'X', 'Y', 'Z']) -> None:
             self.sku = sku
             self.offer_type = offer_type
             self.trigger_quantity = trigger_quantity
@@ -53,54 +55,69 @@ def checkout(skus):
 
     offers = {
         'A': [
-            Offer(sku='A', offer_type=OfferType.LOWER_PRICE_OFFER, trigger_quantity=3, offer_price=130, sku_applied_to='A'),
-            Offer(sku='A', offer_type=OfferType.LOWER_PRICE_OFFER, trigger_quantity=5, offer_price=200, sku_applied_to='A'),
+            Offer(sku='A', offer_type=OfferType.LOWER_PRICE_OFFER,
+                  trigger_quantity=3, offer_price=130, sku_applied_to='A'),
+            Offer(sku='A', offer_type=OfferType.LOWER_PRICE_OFFER,
+                  trigger_quantity=5, offer_price=200, sku_applied_to='A'),
         ],
 
         'B': [
-            Offer(sku='B', offer_type=OfferType.LOWER_PRICE_OFFER, trigger_quantity=2, offer_price=45, sku_applied_to='B'),
+            Offer(sku='B', offer_type=OfferType.LOWER_PRICE_OFFER,
+                  trigger_quantity=2, offer_price=45, sku_applied_to='B'),
             # 2E get one B free
-            Offer(sku='B', offer_type=OfferType.FREE_SKU_OFFER, trigger_quantity=2, offer_price=0, sku_applied_to='E')
+            Offer(sku='B', offer_type=OfferType.FREE_SKU_OFFER,
+                  trigger_quantity=2, offer_price=0, sku_applied_to='E')
         ],
 
         'F': [
             # 2F get one F free
-             Offer(sku='F', offer_type=OfferType.BUY_X_GET_1_FREE, trigger_quantity=3, offer_price=0, sku_applied_to='F', x = 2)
+            Offer(sku='F', offer_type=OfferType.BUY_X_GET_1_FREE,
+                  trigger_quantity=3, offer_price=0, sku_applied_to='F', x=2)
         ],
         'H': [
-            Offer(sku='H', offer_type=OfferType.LOWER_PRICE_OFFER, trigger_quantity=5, offer_price=45, sku_applied_to='H'),
-            Offer(sku='H', offer_type=OfferType.LOWER_PRICE_OFFER, trigger_quantity=10, offer_price=80, sku_applied_to='H'),
+            Offer(sku='H', offer_type=OfferType.LOWER_PRICE_OFFER,
+                  trigger_quantity=5, offer_price=45, sku_applied_to='H'),
+            Offer(sku='H', offer_type=OfferType.LOWER_PRICE_OFFER,
+                  trigger_quantity=10, offer_price=80, sku_applied_to='H'),
         ],
         'K': [
-            Offer(sku='K', offer_type=OfferType.LOWER_PRICE_OFFER, trigger_quantity=2, offer_price=150, sku_applied_to='K'),
+            Offer(sku='K', offer_type=OfferType.LOWER_PRICE_OFFER,
+                  trigger_quantity=2, offer_price=150, sku_applied_to='K'),
         ],
 
         'M': [
             # 3N get one M free
-            Offer(sku='M', offer_type=OfferType.FREE_SKU_OFFER, trigger_quantity=3, offer_price=0, sku_applied_to='N')
+            Offer(sku='M', offer_type=OfferType.FREE_SKU_OFFER,
+                  trigger_quantity=3, offer_price=0, sku_applied_to='N')
         ],
 
         'P': [
-            Offer(sku='P', offer_type=OfferType.LOWER_PRICE_OFFER, trigger_quantity=5, offer_price=200, sku_applied_to='P'),
+            Offer(sku='P', offer_type=OfferType.LOWER_PRICE_OFFER,
+                  trigger_quantity=5, offer_price=200, sku_applied_to='P'),
         ],
 
         'Q': [
             # 3R get one Q free
-            Offer(sku='Q', offer_type=OfferType.FREE_SKU_OFFER, trigger_quantity=3, offer_price=0, sku_applied_to='R'),
-             Offer(sku='Q', offer_type=OfferType.LOWER_PRICE_OFFER, trigger_quantity=3, offer_price=80, sku_applied_to='Q'),
+            Offer(sku='Q', offer_type=OfferType.FREE_SKU_OFFER,
+                  trigger_quantity=3, offer_price=0, sku_applied_to='R'),
+            Offer(sku='Q', offer_type=OfferType.LOWER_PRICE_OFFER,
+                  trigger_quantity=3, offer_price=80, sku_applied_to='Q'),
         ],
 
         'U': [
             # 2F get one F free
-             Offer(sku='U', offer_type=OfferType.BUY_X_GET_1_FREE, trigger_quantity=4, offer_price=0, sku_applied_to='U', x=3)
+            Offer(sku='U', offer_type=OfferType.BUY_X_GET_1_FREE,
+                  trigger_quantity=4, offer_price=0, sku_applied_to='U', x=3)
         ],
 
         'V': [
-            Offer(sku='V', offer_type=OfferType.LOWER_PRICE_OFFER, trigger_quantity=2, offer_price=90, sku_applied_to='V'),
-            Offer(sku='V', offer_type=OfferType.LOWER_PRICE_OFFER, trigger_quantity=3, offer_price=130, sku_applied_to='V'),
+            Offer(sku='V', offer_type=OfferType.LOWER_PRICE_OFFER,
+                  trigger_quantity=2, offer_price=90, sku_applied_to='V'),
+            Offer(sku='V', offer_type=OfferType.LOWER_PRICE_OFFER,
+                  trigger_quantity=3, offer_price=130, sku_applied_to='V'),
         ],
     }
-    
+
     def calculate_total_cost(sku_counts):
         for sku in sku_counts:
             if sku not in price_table:
@@ -110,7 +127,7 @@ def checkout(skus):
 
         for sku in sku_counts:
             quantity_purchased = sku_counts[sku]
-            total_cost += quantity_purchased * price_table[sku]       
+            total_cost += quantity_purchased * price_table[sku]
 
         # calculate reduction in cost
         for sku in sku_counts:
@@ -123,29 +140,29 @@ def checkout(skus):
 
             # order offers by price per unit
             sku_offers.sort(key=lambda x: x.offer_price/x.trigger_quantity)
-                        
+
             for _, offer in enumerate(sku_offers):
                 while sku_counts[sku] > 0:
                     if offer.offer_type == OfferType.LOWER_PRICE_OFFER and sku_counts[sku] >= offer.trigger_quantity:
-                        total_cost = total_cost - (price_table[sku] * offer.trigger_quantity) + offer.offer_price
+                        total_cost = total_cost - \
+                            (price_table[sku] *
+                             offer.trigger_quantity) + offer.offer_price
                         sku_counts[sku] -= offer.trigger_quantity
 
-                    elif offer.offer_type == OfferType.FREE_SKU_OFFER and sku_counts[offer.sku_applied_to] >= offer.trigger_quantity: 
+                    elif offer.offer_type == OfferType.FREE_SKU_OFFER and sku_counts[offer.sku_applied_to] >= offer.trigger_quantity:
                         total_cost = total_cost - price_table[sku]
                         sku_counts[offer.sku_applied_to] -= offer.trigger_quantity
                         sku_counts[sku] -= 1
 
-                    elif offer.offer_type == OfferType.BUY_X_GET_1_FREE and sku_counts[offer.sku_applied_to] >= offer.trigger_quantity: 
+                    elif offer.offer_type == OfferType.BUY_X_GET_1_FREE and sku_counts[offer.sku_applied_to] >= offer.trigger_quantity:
                         total_cost = total_cost - price_table[sku]
                         sku_counts[offer.sku_applied_to] -= offer.x
                         sku_counts[sku] -= 1
-                    
+
                     else:
                         break
 
         return total_cost
-        
 
-    
     # Solution
     return calculate_total_cost(Counter(skus))
