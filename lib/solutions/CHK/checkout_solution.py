2bd361ec78fd98a009fe46b0b53cecfc4e2a1fe0
sbx_user1051 <sbx_user1051@169.254.5.109>
2022-04-21 19:38:34 +0000

    Thu Apr 21 19:38:34 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -18,87 +18,83 @@ def checkout(skus):
             self.offer_price = offer_price
             self.sku_applied_to = sku_applied_to
 
-    class PriceTable:
-        price_table = {
-            'A': 50,
-            'B': 30,
-            'C': 20,
-            'D': 15,
-            'E': 40
-        }
+    price_table = {
+        'A': 50,
+        'B': 30,
+        'C': 20,
+        'D': 15,
+        'E': 40
+    }
 
-        offers = {
-            'A': [
-                Offer(sku='A', offer_type=OfferType.LOWER_PRICE_OFFER, trigger_quantity=3, offer_price=130, sku_applied_to='A'),
-                Offer(sku='A', offer_type=OfferType.LOWER_PRICE_OFFER, trigger_quantity=5, offer_price=200, sku_applied_to='A'),
-            ],
+    offers = {
+        'A': [
+            Offer(sku='A', offer_type=OfferType.LOWER_PRICE_OFFER, trigger_quantity=3, offer_price=130, sku_applied_to='A'),
+            Offer(sku='A', offer_type=OfferType.LOWER_PRICE_OFFER, trigger_quantity=5, offer_price=200, sku_applied_to='A'),
+        ],
 
-            'B': [
-                Offer(sku='B', offer_type=OfferType.LOWER_PRICE_OFFER, trigger_quantity=2, offer_price=45, sku_applied_to='B'),
-            ],
+        'B': [
+            Offer(sku='B', offer_type=OfferType.LOWER_PRICE_OFFER, trigger_quantity=2, offer_price=45, sku_applied_to='B'),
+        ],
 
-            'E': [
-                Offer(sku='E', offer_type=OfferType.FREE_SKU_OFFER, trigger_quantity=2, offer_price=None, sku_applied_to='B')
-            ]
-        }
-        
-        def __init__(self, sku_counts = Dict[str, int]) -> None:
-            self.sku_counts = sku_counts 
+        'E': [
+            Offer(sku='E', offer_type=OfferType.FREE_SKU_OFFER, trigger_quantity=2, offer_price=None, sku_applied_to='B')
+        ]
+    }
+    
+    def calculate_total_cost(sku_counts):
+        total_cost = float('inf')
 
-        def calculate_total_cost(self):
-            total_cost = float('inf')
+        offer_combinations = list(itertools.product(*offers.values()))
 
-            offer_combinations = list(itertools.product(*self.offers.values()))
+        for combo in offer_combinations:
+            total_cost_for_combo = 0
 
-            for combo in offer_combinations:
-                total_cost_for_combo = 0
+            sku_to_offer_map = dict()
+            
+            for offer in combo:
+                sku_to_offer_map[offer.sku] = offer
 
-                sku_to_offer_map = dict()
-                
-                for offer in combo:
-                    sku_to_offer_map[offer.sku] = offer
+            total_costs_per_sku = {
+                'A': 0,
+                'B': 0,
+                'C': 0,
+                'D': 0,
+                'E': 0
+            }
 
-                total_costs_per_sku = {
-                    'A': 0,
-                    'B': 0,
-                    'C': 0,
-                    'D': 0,
-                    'E': 0
-                }
+            # calculate total cost for each sku
+            for sku in sku_counts:
+                quantity_purchased = sku_counts[sku]
 
-                # calculate total cost for each sku
-                for sku in self.sku_counts:
-                    quantity_purchased = self.sku_counts[sku]
+                # check if offer applies to the sku
+                if sku in sku_to_offer_map:
+                    if sku_counts[sku] == 0:
+                        continue
 
-                    # check if offer applies to the sku
-                    if sku in sku_to_offer_map:
-                        if self.sku_counts[sku] == 0:
-                            continue
+                    sku_offer = sku_to_offer_map[sku]
+                    
+                    if sku_offer.offer_type == OfferType.LOWER_PRICE_OFFER:
+                        basic_quantity = quantity_purchased % sku_offer.trigger_quantity
+                        total_costs_per_sku[sku] += basic_quantity * price_table[sku] + ((quantity_purchased - basic_quantity) / sku_offer.trigger_quantity) * sku_offer.offer_price
+                    
+                    elif sku_offer.offer_type == OfferType.FREE_SKU_OFFER:
+                        basic_quantity = quantity_purchased % sku_offer.trigger_quantity
+                        total_costs_per_sku[sku_offer.sku_applied_to] -= ((quantity_purchased - basic_quantity) / sku_offer.trigger_quantity) * price_table[sku_offer.sku_applied_to]
+                        total_costs_per_sku[sku] += quantity_purchased * price_table[sku]      
+                else:
+                    total_costs_per_sku[sku] += quantity_purchased * price_table[sku]
 
-                        sku_offer = sku_to_offer_map[sku]
-                        
-                        if sku_offer.offer_type == OfferType.LOWER_PRICE_OFFER:
-                            basic_quantity = quantity_purchased % sku_offer.trigger_quantity
-                            total_costs_per_sku[sku] += basic_quantity * self.price_table[sku] + ((quantity_purchased - basic_quantity) / sku_offer.trigger_quantity) * sku_offer.offer_price
-                        
-                        elif sku_offer.offer_type == OfferType.FREE_SKU_OFFER:
-                            basic_quantity = quantity_purchased % sku_offer.trigger_quantity
-                            total_costs_per_sku[sku_offer.sku_applied_to] -= ((quantity_purchased - basic_quantity) / sku_offer.trigger_quantity) * self.price_table[sku_offer.sku_applied_to]
-                            total_costs_per_sku[sku] += quantity_purchased * self.price_table[sku]      
-                    else:
-                        total_costs_per_sku[sku] += quantity_purchased * self.price_table[sku]
+            total_cost_for_combo = sum(x if x >= 0 else 0 for x in total_costs_per_sku.values())
 
-                total_cost_for_combo = sum(x if x >= 0 else 0 for x in total_costs_per_sku.values())
+            total_cost = min(total_cost_for_combo, total_cost)
 
-                total_cost = min(total_cost_for_combo, total_cost)
-
-            return total_cost
+        return total_cost
     
     # Solution
     sku_counts = Counter(skus)
-    price_table = PriceTable(sku_counts)
-    return price_table.calculate_total_cost()
+    return calculate_total_cost(sku_counts)
    
+
 
 
 
